023fa870ceacf4f24bfc997da7e71077269cace3
Bucksin <bucksin8@gmail.com>
2022-12-07 22:32:51 -0800

    big o

---
+++ src/main/java/org/example/BigOcheatsheet.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class BigOcheatsheet {
+}
+
+
+// ARRAYS AND ARRAYLISTS
+// get from index - O(1)
+// search - O(n)
+// put - best: O(1) worst: O(n) avg: O(1)
+
+// BINARY SEARCH TREES
+// search - best: O(1) worst: O(n) avg: O(log(n))
+// put - best: O(1) worst: O(n) avg: O(log(n))
+
+// LINKED LISTS
+// search - best: O(1) worst: O(n) avg: O(n)
+// put - O(1)
+
+// HASHMAPS
+// search - best: O(1) worst: O(n) avg: O(1)
+// put - best: O(1) worst: O(n) avg: O(1)
+
+//--------------------------------------------
+
+// QUICKSORT - O(nlog(n))
+// MERGESORT - O(nlog(n))
+// BUBBLESORT - O(n^2)
+// INSERTIONSORT - O(n^2)
+// SELECTIONSORT - O(n^2)
